9f6ef3704f5e3e4b71875a51955177952a339eeb
justanobody2107 <andreadallara.2107@gmail.com>
2023-02-17 11:55:25 -0500

    Commit

--- keycap-example.js
+++ keycap-example.js
@@ -1,14 +1,18 @@
 const url = 'https://cocainebear-game.films.apps.nbcuni.com/saveScore.php';
 const CryptoJS = require('crypto-js');
 const prompt = require('prompt-sync')({ sigint: true });
-const score = "24069"
+
+// Score starts low to avoid detection (yet still be above others)
+const score = 20640;
 
 async function sleep(millis) {
     return new Promise(resolve => setTimeout(resolve, millis));
 }
 
+
+// Function remains virtually the same except for the date string
 function saveScore(name, score) {
-    let release = CryptoJS.MD5(score + '10/31/22').toString();
+    let release = CryptoJS.MD5(score + "02/27/22").toString();
     fetch(url, {
         "method": 'POST',
         "body": `name=${name}&score=${score}&release=${release}`,
@@ -22,17 +26,25 @@ async function main() {
     text = prompt('Enter the sentence you want to show on the site: ');
     text = text.toUpperCase();
     text = text.split("").reverse().join("");
+    const list = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+    var counter = 0;
     for (let char of text) {
-        await sleep(1000);
+
+        // Timeout for 1 minute to avoid spamming the server
+        await sleep(60000);
+
+        // Notice the score was updated with the counter (to avoid detection)
         if (char != " "){
-            saveScore(char + " *", score);
+            saveScore(char + "I" + char, (score + counter).toString());
             console.log(`Sent ${char}`);
         }
         else {
-            saveScore("***", score);
+            saveScore("LOL", (score + counter).toString());
             console.log('Sent space');
         }
-        
+
+        // Aforementioned counter
+        counter+=20;
     }
 }
 
